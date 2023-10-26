92ba590089ec4827bddb8490b5cc99f87b1ce0e0
vanillaicedlatte <jspann11@yahoo.com>
2023-10-26 11:52:21 -0500

    Completed: 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    for (i = 0; i < num; i++) {
+        newString = newString.concat(string);
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line

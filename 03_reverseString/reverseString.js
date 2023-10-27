805d9fcfff769e0b3313d25475584f792e25eb88
vanillaicedlatte <jspann11@yahoo.com>
2023-10-26 21:19:17 -0500

    Completed: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+    stringSplit = myString.split("").reverse().join('');
+    console.log(stringSplit);
+    return stringSplit;
 };
+
+reverseString("Hello, World!");
 
 // Do not edit below this line
 module.exports = reverseString;
